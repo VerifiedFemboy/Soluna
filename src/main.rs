c243735fe408eb11540b8a8db7c7bdec0f8892f2
verifiedfemboy <maciekplaygamesyt@gmail.com>
2024-09-18 17:06:53 +0200

    Some changes for better work

--- src/main.rs
+++ src/main.rs
@@ -11,21 +11,30 @@ async fn main() {
 
     let current_ip: serde_json::Value = serde_json::from_str(&current_ip).unwrap();
 
+    let geolocation = geolocation::find(current_ip["ip"].as_str().unwrap()).unwrap();
+    let longitude = geolocation.longitude.parse::<f64>().unwrap();
+    let latitude = geolocation.latitude.parse::<f64>().unwrap();
+
     loop {
         print!("\x1B[2J\x1B[1;1H");
-        std::thread::sleep(std::time::Duration::from_millis(500));
+        std::thread::sleep(std::time::Duration::from_millis(100));
         let current_time: chrono::DateTime<chrono::Local> = chrono::Local::now();
         let julian_day = calculations::julian_day(current_time);
         let time = current_time.hour() as f64 + current_time.minute() as f64 / 60.0 + current_time.second() as f64 / 3600.0;
 
-        let longitude = geolocation::find(current_ip["ip"].as_str().unwrap()).unwrap().longitude.parse::<f64>().unwrap();
+        let solar_declination = calculations::solar_declination(julian_day);
+        let solar_hour_angle = calculations::solar_hour_angle(time, longitude);
 
-        let declination = calculations::solar_declination(julian_day);
-        let hour_angle = calculations::solar_hour_angle(time, longitude);
+        let current_time_format: String = current_time.format("%H:%M:%S | %m-%d-%Y").to_string();
 
-        println!("Current Time (LOCAL): {}", current_time);
-        println!("Julian Day: {}", julian_day);
-        println!("Declination: {}", declination);
-        println!("Hour Angle: {}", hour_angle);
+        println!("Current Time (LOCAL): {}", current_time_format);
+        println!("  Location Information");
+        println!("      Latitude: {}", latitude);
+        println!("      Longitude: {}", longitude);
+        println!("");
+        println!("  Calculated Information");
+        println!("      Julian Day: {}", julian_day);
+        println!("      Solar Declination: {}", solar_declination);
+        println!("      Solar Hour Angle: {}", solar_hour_angle);
     }
 }
