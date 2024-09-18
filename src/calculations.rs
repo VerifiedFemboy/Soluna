0e8f2d55c5df500cb3d47f57da7e250bc63bb6e2
verifiedfemboy <maciekplaygamesyt@gmail.com>
2024-09-18 17:56:11 +0200

    some amendments

--- src/calculations.rs
+++ src/calculations.rs
@@ -3,16 +3,16 @@ use chrono::{DateTime, Datelike, Local, Timelike};
 pub fn solar_declination(julian_day: f64) -> f64 {
     let n = julian_day - 1.0;
     let angle = 23.44 * (std::f64::consts::PI / 180.0);
-    let declination = angle * (360.0 * (n + 10.0) / 365.0).cos();
+    let declination = (angle * (360.0 * (n + 10.0) / 365.0).to_radians().sin()).to_degrees();
     declination
 }
 
 pub fn solar_hour_angle(time: f64, longitude: f64) -> f64 {
     let hour_angle = (time - 12.0) * 15.0 + longitude;
-    hour_angle
+    hour_angle.to_degrees()
 }
 
-pub fn julian_day(local: DateTime<Local>) -> f64 {
+pub fn calculate_julian_day(local: DateTime<Local>) -> f64 {
     let year = local.year();
     let month = local.month();
     let day = local.day();
@@ -23,4 +23,23 @@ pub fn julian_day(local: DateTime<Local>) -> f64 {
     let millisecond = millisecond as f64 / 1000.0;
     let julian_day = 367.0 * year as f64 - (7.0 * (year as f64 + ((month as f64 + 9.0) / 12.0)) / 4.0).floor() + (275.0 * month as f64 / 9.0).floor() + day as f64 + 1721013.5 + ((hour as f64 + (minute as f64 + (second as f64 + millisecond) / 60.0) / 60.0) / 24.0);
     julian_day
+}
+
+pub fn solar_ecliptic_position(day_of_year: f64) -> f64 {
+    
+    let l0 = 280.46; //in degrees
+    
+    let days_since_march = day_of_year - 80.0; //March 21st is the 80th day of the year
+    
+    let daily_angle = 360.0 / 365.25; //365.25 days in a year
+    
+    let mut lambda_s = l0 + daily_angle * days_since_march; //in degrees
+    
+    if lambda_s > 360.0 {
+        lambda_s -= 360.0;
+    } else if lambda_s < 0.0 {
+        lambda_s += 360.0;
+    }
+    
+    lambda_s
 }
--- src/main.rs
+++ src/main.rs
@@ -19,11 +19,12 @@ async fn main() {
         print!("\x1B[2J\x1B[1;1H");
         std::thread::sleep(std::time::Duration::from_millis(100));
         let current_time: chrono::DateTime<chrono::Local> = chrono::Local::now();
-        let julian_day = calculations::julian_day(current_time);
+        let julian_day = calculations::calculate_julian_day(current_time);
         let time = current_time.hour() as f64 + current_time.minute() as f64 / 60.0 + current_time.second() as f64 / 3600.0;
 
         let solar_declination = calculations::solar_declination(julian_day);
         let solar_hour_angle = calculations::solar_hour_angle(time, longitude);
+        let solar_ecliptic_position = calculations::solar_ecliptic_position(julian_day - 1721013.5);
 
         let current_time_format: String = current_time.format("%H:%M:%S | %m-%d-%Y").to_string();
 
@@ -36,5 +37,6 @@ async fn main() {
         println!("      Julian Day: {}", julian_day);
         println!("      Solar Declination: {}", solar_declination);
         println!("      Solar Hour Angle: {}", solar_hour_angle);
+        println!("      Solar Ecliptic Position: {}", solar_ecliptic_position);
     }
 }
