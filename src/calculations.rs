da25fd71100ecb91f9b52993748cbedbd83ab0b0
verifiedfemboy <maciekplaygamesyt@gmail.com>
2024-09-18 09:29:01 +0200

    Added current geolocation & current local system time

--- src/calculations.rs
+++ src/calculations.rs
@@ -1,4 +1,4 @@
-use chrono::{DateTime, Datelike, Timelike, Utc};
+use chrono::{DateTime, Datelike, Local, Timelike, Utc};
 
 pub fn solar_declination(julian_day: f64) -> f64 {
     let n = julian_day - 1.0;
@@ -15,14 +15,14 @@ pub fn solar_hour_angle(julian_day: f64, time: f64, longitude: f64) -> f64 {
     hour_angle
 }
 
-pub fn julian_day(utc: DateTime<Utc>) -> f64 {
-    let year = utc.year();
-    let month = utc.month();
-    let day = utc.day();
-    let hour = utc.hour();
-    let minute = utc.minute();
-    let second = utc.second();
-    let millisecond = utc.timestamp_subsec_millis();
+pub fn julian_day(local: DateTime<Local>) -> f64 {
+    let year = local.year();
+    let month = local.month();
+    let day = local.day();
+    let hour = local.hour();
+    let minute = local.minute();
+    let second = local.second();
+    let millisecond = local.timestamp_subsec_millis();
     let millisecond = millisecond as f64 / 1000.0;
     let julian_day = 367.0 * year as f64 - (7.0 * (year as f64 + ((month as f64 + 9.0) / 12.0)) / 4.0).floor() + (275.0 * month as f64 / 9.0).floor() + day as f64 + 1721013.5 + ((hour as f64 + (minute as f64 + (second as f64 + millisecond) / 60.0) / 60.0) / 24.0);
     julian_day
--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,28 @@ use chrono::Timelike;
 
 mod calculations;
 
-fn main() {
+#[tokio::main]
+async fn main() {
+
+    let current_ip = reqwest::get("https://api64.ipify.org?format=json")
+    .await
+    .unwrap().text().await.unwrap();
+
+    let current_ip: serde_json::Value = serde_json::from_str(&current_ip).unwrap();
+
     loop {
         print!("\x1B[2J\x1B[1;1H");
         std::thread::sleep(std::time::Duration::from_millis(500));
-        let current_time: chrono::DateTime<chrono::Utc> = chrono::Utc::now();
+        let current_time: chrono::DateTime<chrono::Local> = chrono::Local::now();
         let julian_day = calculations::julian_day(current_time);
         let time = current_time.hour() as f64 + current_time.minute() as f64 / 60.0 + current_time.second() as f64 / 3600.0;
-        let longitude = 15.0;
+
+        let longitude = geolocation::find(current_ip["ip"].as_str().unwrap()).unwrap().longitude.parse::<f64>().unwrap();
 
         let declination = calculations::solar_declination(julian_day);
         let hour_angle = calculations::solar_hour_angle(julian_day, time, longitude);
 
-        println!("Current Time (UTC): {}", current_time);
+        println!("Current Time (LOCAL): {}", current_time);
         println!("Julian Day: {}", julian_day);
         println!("Declination: {}", declination);
         println!("Hour Angle: {}", hour_angle);
